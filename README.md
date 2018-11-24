763e8196fa68b3934f2d8323772a8c187f6da445
Lingyu Zhou <zhoulytwin@gmail.com>
2018-11-24 15:07:21 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Grid Card Manager
+Simple, CLI grid card manager. The grid card data is stored with symmetric encryption scheme with salt in .config folder.
+
+## Usage:
+```
+  <script>.py --columns <columns> --rows <rows> --grid <grid>
+  <script>.py --retrieve <cells>
+  <script>.py --interactive
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+setuptools.setup(
+  name='GridCardManager',
+  version='0.1',
+  description='BCH grid card manager',
+  url='http://github.com/zhoulytwinyu/GridCardManager',
+  author='Lingyu Zhou',
+  author_email='zhoulytwin@gmail.com',
+  license='GPLv3',
+  packages=['GridCardManager'],
+  zip_safe=False,
+  scripts=['bin/GridCardManager'],
+)
